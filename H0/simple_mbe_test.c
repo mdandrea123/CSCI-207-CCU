610c94dba2be6e2f34e7bfd3b824a742c8904399
Michael Dandrea <michaeldandrea@Michaels-MBP.lan>
2023-11-23 20:01:15 -0500

    Updated Files

--- H0/simple_mbe_test.c
+++ H0/simple_mbe_test.c
@@ -11,11 +11,17 @@ int main(int argc, char **argv){
     long long trials = 0;
     int pFlag = 0;
     double start, end, elapsed;
+
     parseMBE(argc, argv, &trials, &pFlag);
+
     GET_TIME(start);
+
     mbeTest(trials, pFlag);
+
     GET_TIME(end);
+
     elapsed = end - start;
     printf("This program took: %e seconds.\n", elapsed);
+
     return 0;
 }
